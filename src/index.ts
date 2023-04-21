d4e9f037412574f35b32c2fb8794e3c32d6660c4
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-04-21 23:20:14 +0900

    ブラウザがどの配信サイトを開いているかの区別を別のファイルに抜き出した

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,9 @@
-import { LiveSite } from "./class/liveSite";
-import { Openrec } from "./class/openrec"
-import { Twitch } from "./class/twitch"
-import { YouTube } from "./class/YouTube"
+import { checkStreamSite } from "./checkStreamSite";
 
 // サーバーのポート
 const port = "10010";
 // サーバーのURL
 const serverUrl = `http://localhost:${port}/`;
 
-// extractComment() の記述を一箇所にまとめたいのでここで変数を宣言する
-let liveSite: LiveSite;
-
-// どのサイトに接続しているかの判断
-switch (location.hostname) {
-    case "www.openrec.tv":
-        console.log(`Openrecです`)
-        liveSite = new Openrec(serverUrl)
-        break;
-    case "www.twitch.tv":
-        console.log("Twitchです")
-        liveSite = new Twitch(serverUrl)
-        break;
-    case "www.youtube.com":
-        console.log("YouTubeです");
-        liveSite = new YouTube(serverUrl);
-        break;
-    default:
-        console.log("拡張機能の使用に失敗しました");
-        break;
-}
+const liveSite = checkStreamSite(location.hostname, serverUrl);
 if (liveSite != null) liveSite.extractComment();
