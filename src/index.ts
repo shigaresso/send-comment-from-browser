c69a65b2aed7a999551c941b8c92ba08c3791452
shigaresso <70837100+shigaresso@users.noreply.github.com>
2024-06-16 01:27:22 +0900

    タブに対してメッセージを送れるようになった。（その代わり URL に対してではなく、タブに対して送信することになる。）

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,30 @@ import {
 // 実行したい処理の一連のまとめ
 const sendCommentToBrowser = async (): Promise<void> => {
   // VSCode の拡張機能 Live Server を使った時のポート番号
-//   const port: string = "5500";
-//   const origin: string = `http://localhost:${port}`;
-//   const urlOpenedByBrowser: string = `${origin}/index.html`;
+  const port: string = "5500";
+  const origin: string = `http://localhost:${port}`;
+  const urlOpenedByBrowser: string = `${origin}/index.html`;
 
-//   const windowSubmittedComment = getHtmlSubmittedComment(urlOpenedByBrowser);
+  const windowSubmittedComment = getHtmlSubmittedComment(urlOpenedByBrowser);
 
   const commentArea = await getCommentArea(location.hostname);
 
+  const sendComment = (message: any) =>
+    windowSubmittedComment.postMessage({ message: message }, origin);
+  observerStart(commentArea, sendComment);
+};
+
+const observerStart = (commentArea: HTMLElement, action: any): void => {
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       mutation.addedNodes.forEach((node) => {
-        console.log(node);
+        action(`${node}`);
       });
     });
   });
   // オブザーバの設定
   const config = { childList: true };
   observer.observe(commentArea, config);
-
-//   windowSubmittedComment.postMessage({}, origin);
 };
 
 sendCommentToBrowser();
